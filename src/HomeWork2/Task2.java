33d78e1b3df349d6e04be8bc6d4d1f5ddbef2b81
Anisk1993 <ilya_aniskevich@mail.ru>
2020-06-21 14:32:00 +0300

    Задача №2

--- src/HomeWork2/Task2.java
+++ src/HomeWork2/Task2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        System.out.print("Введите месяц : ");
+        System.out.print("Введите номер месяца : ");
         int mon=in.nextInt();
         System.out.print("Введите день : ");
         int day=in.nextInt();
