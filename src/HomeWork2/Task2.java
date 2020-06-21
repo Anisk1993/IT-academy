60884ebf074b96fb95683f2abb9c97e9dd2caa56
Anisk1993 <ilya_aniskevich@mail.ru>
2020-06-21 14:28:18 +0300

    Задача №2

---
+++ src/HomeWork2/Task2.java
@@ -0,0 +1,42 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Введите месяц : ");
+        int mon=in.nextInt();
+        System.out.print("Введите день : ");
+        int day=in.nextInt();
+
+        int[][]month=new int[12][];
+        months(month);
+        int i, j;
+        for (i = 0; i <month.length ; i++)
+            for (j = 0; j < month[i].length; j++) {
+                month[i][j] = j + 1;
+
+            }
+        if(month[mon-1][month[mon-1].length-1]==day)
+            System.out.println("Такой день в этом месяце есть");
+            else System.out.println("Такого дня нет");
+
+    }
+
+    private static void months(int[][] month) {
+        month[0]=new int[31];
+        month[1]=new int[28];
+        month[2]=new int[31];
+        month[3]=new int[30];
+        month[4]=new int[31];
+        month[5]=new int[30];
+        month[6]=new int[31];
+        month[7]=new int[31];
+        month[8]=new int[30];
+        month[9]=new int[31];
+        month[10]=new int[30];
+        month[11]=new int[31];
+    }
+
+}
