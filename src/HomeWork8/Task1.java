2e69cb17de7aa311c7be528b4f2496107c4f4e56
Anisk1993 <ilya_aniskevich@mail.ru>
2020-08-05 23:13:57 +0300

    HomeWork8

---
+++ src/HomeWork8/Task1.java
@@ -0,0 +1,22 @@
+package HomeWork8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        String stringBuilder = new String("The new license permits certain uses, " +
+                "such as personal use and development use, at no cost - but other uses authorized " +
+                "under prior Oracle Java licenses may no longer be available");
+
+        String[] wordsArray = stringBuilder.split(" ");
+        List<String>words = Arrays.asList(wordsArray);
+        words.stream()
+                .map(String::toLowerCase)
+                .filter(word -> word.startsWith("a"))
+                .forEach(System.out::println);
+
+
+    }
+}
